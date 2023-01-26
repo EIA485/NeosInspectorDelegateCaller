221d5964725de3ad7da0e42c9116e0ad058ca171
Sam Scott <samueltscott@gmail.com>
2023-01-26 17:13:34 +0000

    Tabify whole document

--- InspectorDelegateCaller/InspectorDelegateCaller.cs
+++ InspectorDelegateCaller/InspectorDelegateCaller.cs
@@ -25,8 +25,8 @@ namespace InspectorDelegateCaller
 		[AutoRegisterConfigKey] static ModConfigurationKey<bool> Key_Buttons = new("buttons", "show callable buttons in inspectors", () => false);
 		[AutoRegisterConfigKey] static ModConfigurationKey<bool> Key_ArgButtons = new("argButtons", "show any button with arguments in inspectors", () => true);
 
-        [AutoRegisterConfigKey] static ModConfigurationKey<bool> Key_ShowSlotDestroy = new("showSlotDestroy", "show the slot destroy button in inspectors", () => true);
-        static ModConfiguration config;
+		[AutoRegisterConfigKey] static ModConfigurationKey<bool> Key_ShowSlotDestroy = new("showSlotDestroy", "show the slot destroy button in inspectors", () => true);
+		static ModConfiguration config;
 
 		public override void OnEngineInit()
 		{
@@ -40,22 +40,22 @@ namespace InspectorDelegateCaller
 		{
 			static void Postfix(Worker worker, UIBuilder ui)
 			{
-                
-                foreach (var m in worker.GetType().GetMethods(BindingFlags.FlattenHierarchy | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static))
+
+				foreach (var m in worker.GetType().GetMethods(BindingFlags.FlattenHierarchy | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static))
 				{
 					var param = m.GetParameters();
-                    
-                    if (m.ReturnType == typeof(void))
+
+					if (m.ReturnType == typeof(void))
 					{
 						switch (param.Length)
 						{
 							case 0: //could have some branching mess here. may be marginally faster
 								if (m.CustomAttributes.Any((a) => (a.AttributeType == typeof(SyncMethod) && config.GetValue(Key_Action)) || (a.AttributeType.BaseType == typeof(SyncMethod) && config.GetValue(Key_SubAction))))
 								{
-                                    LocaleString str = m.Name;
+									LocaleString str = m.Name;
 
 									// check for the slot destroy button
-                                    if (str == "Destroy" && worker.GetType().FullName == "FrooxEngine.Slot" && config.GetValue(Key_ShowSlotDestroy) == false) break;
+									if (str == "Destroy" && worker.GetType().FullName == "FrooxEngine.Slot" && config.GetValue(Key_ShowSlotDestroy) == false) break;
 
 									var b = ui.Button(in str);
 									b.Slot.AttachComponent<ButtonActionTrigger>().OnPressed.Target = (Action)m.CreateDelegate(typeof(Action), worker);
@@ -64,7 +64,7 @@ namespace InspectorDelegateCaller
 							case 1:
 								if (config.GetValue(Key_ArgAction) && hasSyncMethod(m))
 								{
-                                    var p = param[0];
+									var p = param[0];
 									var pt = p.ParameterType;
 									if (pt.GetInterfaces().Contains(typeof(IWorldElement)))
 										actionCallbackwitharg(true, worker, ui, m, p, pt);
@@ -75,14 +75,14 @@ namespace InspectorDelegateCaller
 							case 2:
 								if (config.GetValue(Key_Buttons) && isButtonDelegate(param) && hasSyncMethod(m))
 								{
-                                    LocaleString str = m.Name;
-                                    var b = ui.Button(in str).Pressed.Target = (ButtonEventHandler)m.CreateDelegate(typeof(ButtonEventHandler), worker);
+									LocaleString str = m.Name;
+									var b = ui.Button(in str).Pressed.Target = (ButtonEventHandler)m.CreateDelegate(typeof(ButtonEventHandler), worker);
 								}
 								break;
 							case 3:
 								if (config.GetValue(Key_ArgButtons) && isButtonDelegate(param) && hasSyncMethod(m))
 								{
-                                    var p = param[2];
+									var p = param[2];
 									var pt = p.ParameterType;
 									if (pt.GetInterfaces().Contains(typeof(IWorldElement)))
 										buttonCallbackwitharg(typeof(ButtonRefRelay<>), worker, ui, m, p, pt);
@@ -103,7 +103,7 @@ namespace InspectorDelegateCaller
 		{
 			ui.HorizontalLayout();
 			LocaleString str = m.Name;
-            var b = ui.Button(in str);
+			var b = ui.Button(in str);
 			var apt = typeof(Action<>).MakeGenericType(pt);
 			Type t = (isRef ? typeof(CallbackRefArgument<>) : typeof(CallbackValueArgument<>)).MakeGenericType(pt);
 			var c = b.Slot.AttachComponent(t);
@@ -118,7 +118,7 @@ namespace InspectorDelegateCaller
 		{
 			ui.HorizontalLayout();
 			LocaleString str = m.Name;
-            var b = ui.Button(in str);
+			var b = ui.Button(in str);
 			var bpt = typeof(ButtonEventHandler<>).MakeGenericType(pt);
 			Type t = genType.MakeGenericType(pt);
 			var c = b.Slot.AttachComponent(t);
